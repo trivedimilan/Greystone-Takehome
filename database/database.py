fc08e831f94c45bd2a83e5e09920659c8e3a9b7b
Milan <trivedimilan*@gmail.com>
2025-09-14 16:01:32 -0400

    built connection to sqlite and initial data models

---
+++ database/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Example for SQLite
+DATABASE_URL = "sqlite:///./database.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Create tables
+def create_tables():
+    Base.metadata.create_all(bind=engine)
